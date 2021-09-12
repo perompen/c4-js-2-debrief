868f4c0866915aa28198d793e158f4067cce32f9
Peter Pen <officialpeterp@yahoo.com>
2021-09-12 15:09:24 -0700

    I have made an edit to a file!

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 // 1. This link contains instructions for completing #1:  https://docs.google.com/document/d/1ACw6ILG_rk66ukkkS_84LWiH63hovGnEOKPNtcvRGlg/edit?usp=sharing
 
-    //A. You will be creating a Random Exercise Generator. 
-        //Build an array with the items listed below in it. Then create a function (using regular function syntax) that will select a 
-        //specified number of exercises (number will be passed into the function) and prints the random exercises to the console. 
-        //It is ok if an exercise appears more than once in the output.
-   // B. Once #A is complete, create a function with the same functionality as #1, but this time using an arrow function.
+//A. You will be creating a Random Exercise Generator. 
+//Build an array with the items listed below in it. Then create a function (using regular function syntax) that will select a 
+//specified number of exercises (number will be passed into the function) and prints the random exercises to the console. 
+//It is ok if an exercise appears more than once in the output.
+// B. Once #A is complete, create a function with the same functionality as #1, but this time using an arrow function.
 
-    //Array items: Push-ups, Sit-ups, Bear Crawls, 1 mile run, 60 second plank, Burpees, Jumping Jacks, Squats, Weighted Squats, Inch Worms
+//Array items: Push-ups, Sit-ups, Bear Crawls, 1 mile run, 60 second plank, Burpees, Jumping Jacks, Squats, Weighted Squats, Inch Worms
 
 //Write your code below this line:
 
+const exercises = ['Push-ups', 'Sit-ups', 'Bear Crawls', '1 mile run',
+    '60 second plant', 'Burpees', 'Jumping Jacks', 'Squats', 'Weight Squats', 'Inch Worms']
 
+// Regular function
+function randomExercises(userInput) {
+    for (let i = 0; i < userInput; i++) {
+        const index = Math.floor(Math.random() * exercises.length)
+        console.log(exercises[index]);
+    }
+}
+randomExercises(4);
 
-
+// Arrow function
+// const randomExercises = userInput => {
+//     for (let i = 0; i < userInput; i++) {
+//         const index = Math.floor(Math.random() * exercises.length)
+//     console.log(exercises[index]);
+//     }
+// }
+// randomExercises(4);
 
 // 2. GenBuzz 
 //    * Using the requirements below, write a function that prints numbers from 1 to an upper limit.That upper limit should be passed into your function as a parameter.
@@ -27,8 +44,26 @@
 
 //Write your code below this line
 
+// Arrow function
+const myNumber = 1;
 
+const GenBuzz = () => {
+    for (let i = 1; i <= 15; i++) {
 
+        if (typeof myNumber !== 'number') {
+            console.log('This is not a number');
+        } else if (i % 15 === 0) {
+            console.log('GenBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Gen');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+GenBuzz();
 
 // 3. E-Commerce Item list
 //     * Using the requiremets below to create a function that takes in an item and prints the price to the console.
@@ -44,3 +79,27 @@
 //      * If the variable input is not an item in the store, then print 'Invalid Item' to the console.
 
 //Write your code below this line:
+
+// Arrow function
+const price = (items) => {
+
+    switch (items) {
+        case 'shoes':
+            console.log('Shoes are $50');
+            break;
+        case 'jeans':
+            console.log('Jeans are $25');
+            break;
+        case 'hat':
+            console.log('Hat is $12');
+            break;
+        case 'socks':
+            console.log('Socks are $2');
+            break;
+        default:
+            console.log('Invalid item');
+            break;
+    }
+}
+
+price('shoes');
